71f2bd50e4721528cf8f972e368b116dd964fa71
masx200 <masx200@qq.com>
2021-06-18 13:56:18 +0800

    Update http-auth.ts

--- src/http-auth.ts
+++ src/http-auth.ts
@@ -1,9 +1,36 @@
-export function httpauth(options: {}) {
+import { v2 as webdav } from "webdav-server";
+import httpauth from "http-auth";
+
+export function createhttpauth(options: {
+    user: string;
+    pass: string;
+    authentication: "HTTPDigestAuthentication" | "HTTPBasicAuthentication";
+}) {
+    const auth =
+        options.authentication === "HTTPBasicAuthentication"
+            ? httpauth.basic(
+                  {
+                      realm: "Default realm",
+                  },
+                  (username, password, callback) => {
+                      // Custom authentication method.
+                      callback(
+                          username === options.user &&
+                              password === options.pass,
+                      );
+                  },
+              )
+            : httpauth.digest(
+                  {
+                      realm: "Default realm",
+                  },
+                  (username, callback) => {
+                      callback();
+                  },
+              );
     return (ctx: webdav.HTTPRequestContext, next: () => void) => {
         auth.check((req, res) => {
             next();
         })(ctx.request, ctx.response);
     };
 }
-import { v2 as webdav } from "webdav-server";
-import auth from "http-auth";
